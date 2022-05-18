88632312002679c951897531c564124112aeb665
Mike Rousos <mikerou@microsoft.com>
2022-05-18 15:16:57 -0400

    Mark an endpoint authorized to test challenge scenarios

--- samples/MvcCoreApp/Controllers/HomeController.cs
+++ samples/MvcCoreApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MvcCoreApp.Models;
 using System.Diagnostics;
 
@@ -18,6 +19,7 @@ namespace MvcCoreApp.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
