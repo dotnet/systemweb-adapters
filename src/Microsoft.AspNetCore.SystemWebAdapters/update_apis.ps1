fbe0747535a1e0d135d4347055ce68a1cca722ec
Taylor Southwick <tasou@microsoft.com>
2022-05-24 14:02:31 -0700

    Remove .NET Core 3.1 from support

--- src/Microsoft.AspNetCore.SystemWebAdapters/update_apis.ps1
+++ src/Microsoft.AspNetCore.SystemWebAdapters/update_apis.ps1
@@ -1,7 +1,7 @@
 ## Running this script will update the type forwards and .NET Standard APIs
 
-dotnet build --no-incremental .\Microsoft.AspNetCore.SystemWebAdapters.csproj -f netcoreapp3.1 /p:GenerateStandard=true
-dotnet build --no-incremental .\Microsoft.AspNetCore.SystemWebAdapters.csproj -f netcoreapp3.1 /p:GenerateTypeForwards=true
+dotnet build --no-incremental .\Microsoft.AspNetCore.SystemWebAdapters.csproj -f net6.0 /p:GenerateStandard=true
+dotnet build --no-incremental .\Microsoft.AspNetCore.SystemWebAdapters.csproj -f net6.0 /p:GenerateTypeForwards=true
 
 # Script will have an error if there are git changes
 if(git status --porcelain){
