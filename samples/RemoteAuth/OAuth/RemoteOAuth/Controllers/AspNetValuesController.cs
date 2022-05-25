b3acf73bee4175ab5168133c08a372e3aee9a9f7
Mike Rousos <Mike@MJRousos.com>
2022-05-25 14:08:27 -0400

    Add OIDC B2C auth to OAuth sample

--- samples/RemoteAuth/OAuth/RemoteOAuth/Controllers/AspNetValuesController.cs
+++ samples/RemoteAuth/OAuth/RemoteOAuth/Controllers/AspNetValuesController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Web.Http;
 
 namespace RemoteOAuth.Controllers
@@ -6,9 +7,17 @@ namespace RemoteOAuth.Controllers
     public class AspNetValuesController : ApiController
     {
         // GET api/values
+        [Authorize]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "Hello from ASP.NET" };
+            yield return User.Identity.Name;
+            if (User is ClaimsPrincipal principal)
+            {
+                foreach (var claim in principal.Claims)
+                {
+                    yield return $"{claim.Type}: {claim.Value}";
+                }
+            }
         }
 
         // GET api/values/5
