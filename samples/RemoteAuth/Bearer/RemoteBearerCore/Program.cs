6db7993a638452319488c2acf4b1d4568cdd70c1
Mike Rousos <Mike@MJRousos.com>
2022-06-08 12:26:28 -0400

    Clarify the random true argument in the remote auth sample app.

--- samples/RemoteAuth/Bearer/RemoteBearerCore/Program.cs
+++ samples/RemoteAuth/Bearer/RemoteBearerCore/Program.cs
@@ -8,7 +8,11 @@ builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSecti
 builder.Services.AddControllers();
 
 builder.Services.AddSystemWebAdapters()
-    .AddRemoteAppAuthentication(true, options =>
+    // This registers the remote app authentication handler. The boolean argument indicates whether remote app auth
+    // should be the default scheme. If it is set to false, HTTP requests to authenticate will only be made for
+    // endpoints that actually need that behavior, but it is then necessary to annotate endpoints requiring remote app
+    // auth with [Authorize(AuthenticationSchemes = RemoteAppAuthenticationDefaults.AuthenticationScheme)] or something similar.
+    .AddRemoteAppAuthentication(isDefaultScheme: true, options =>
     {
         options.RemoteServiceOptions.RemoteAppUrl = new(builder.Configuration["ReverseProxy:Clusters:fallbackCluster:Destinations:fallbackApp:Address"]);
 
