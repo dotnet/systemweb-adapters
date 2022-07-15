d926477a44a0299a3b53588b29ad941652ad938d
Mike Rousos <Mike@MJRousos.com>
2022-07-15 18:11:39 -0400

    Remove workaround from sample that's no longer necessary

--- samples/RemoteAuth/OIDC/OIDCAuth/Controllers/AccountController.cs
+++ samples/RemoteAuth/OIDC/OIDCAuth/Controllers/AccountController.cs
@@ -23,9 +23,7 @@ namespace OIDCAuth.Controllers
 
         public void SignOut()
         {
-            // This has been updated to redirect back to the ASP.NET Core app in our incremental migration scenario
-            // rather than redirecting back to the ASP.NET app directly.
-            string callbackUrl = $"{GetScheme(HttpContext.Request)}://{GetHost(HttpContext.Request)}/Account/SignOutCallback";
+            string callbackUrl = Url.Action("SignOutCallback", "Account", routeValues: null, protocol: Request.Url.Scheme);
 
             HttpContext.GetOwinContext().Authentication.SignOut(
                 new AuthenticationProperties { RedirectUri = callbackUrl },
@@ -42,12 +40,5 @@ namespace OIDCAuth.Controllers
 
             return View();
         }
-
-        private static string GetHost(HttpRequestBase request) =>
-            request.Headers["x-forwarded-host"]
-                ?? (request.Url.IsDefaultPort ? request.Url.Host : $"{request.Url.Host}:{request.Url.Port}");
-
-        private static string GetScheme(HttpRequestBase request) =>
-            request.Headers["x-forwarded-proto"] ?? request.Url.Scheme;
     }
 }
