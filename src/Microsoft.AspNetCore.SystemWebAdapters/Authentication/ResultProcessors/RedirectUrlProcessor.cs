b192fe2a49078c24494ec9cbab752d55a27659ed
Mike Rousos <Mike@MJRousos.com>
2022-07-18 11:58:46 -0400

    Re-add QueryString redirect URL processor for identity/cookie scenarios
    This was removed when we were considering a global return url query
    string in all authenticate flows but now that we're not using that
    approach, this needs added back.

--- src/Microsoft.AspNetCore.SystemWebAdapters/Authentication/ResultProcessors/RedirectUrlProcessor.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters/Authentication/ResultProcessors/RedirectUrlProcessor.cs
@@ -7,21 +7,24 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace Microsoft.AspNetCore.SystemWebAdapters.Authentication.ResultProcessors;
 
 /// <summary>
 /// Processes remote authentication results to fix-up 'ReturnUrl' parameters
-/// given in redirect responses so that they will return the user to the
-/// ASP.NET Core app rather than URLs with the original ASP.NET app's host.
+/// given in redirect responses so that they will return the user to the correct
+/// original URL instead of to the remote authentication endpoint.
 /// </summary>
 internal class RedirectUrlProcessor : IRemoteAppAuthenticationResultProcessor
 {
     private const string LocationHeaderName = "Location";
+    private const string ReturnUrlQueryStringName = "ReturnUrl";
 
     /// <summary>
     /// Updates response headers in a remote authentication result so that any
-    /// 'Location' headers will redirect to the correct (ASP.NET Core) host.
+    /// 'Location' headers will include a ReturnUrl query string corresponding to the
+    /// URL of the original request rather than the URL of the remote authentication endpoint.
     /// </summary>
     /// <param name="result">The remote app authentication result to be updated so that redirects point to the right locations.</param>
     /// <param name="context">The HTTP context containing the original request that prompted authentication.</param>
@@ -29,7 +32,7 @@ internal class RedirectUrlProcessor : IRemoteAppAuthenticationResultProcessor
     {
         if (result.ResponseHeaders.TryGetValue(LocationHeaderName, out var locationHeaders))
         {
-            // Look for any Location headers
+            // Look for any Location headers with URLs including a ReturnUrl query string as their value
             var processedRedirectLocations = new List<string>();
             for (var i = 0; i < locationHeaders.Count; i++)
             {
@@ -39,6 +42,7 @@ internal class RedirectUrlProcessor : IRemoteAppAuthenticationResultProcessor
                     // Update the host for any Location headers so that if they point to a URL
                     // in the old app they instead use the new app's host.
                     redirectLocation = UpdateHost(redirectLocation, result.AuthenticationRequest);
+                    redirectLocation = UpdateQueryStrings(redirectLocation, context.Request);
                     processedRedirectLocations.Add(redirectLocation.ToString());
                 }
             }
@@ -47,6 +51,30 @@ internal class RedirectUrlProcessor : IRemoteAppAuthenticationResultProcessor
         }
 
         return Task.CompletedTask;
+    }
+
+    // Updates the query strings of the redirect URI to redirect back to the current request's URI
+    // rather than to the URI of the authenticate request that led to the redirect response.
+    private static Uri UpdateQueryStrings(Uri redirectLocation, HttpRequest originalRequestPath)
+    {
+        if (!redirectLocation.IsAbsoluteUri)
+        {
+            redirectLocation = GetAbsoluteUri(redirectLocation, originalRequestPath);
+        }
+
+        var queryStrings = QueryHelpers.ParseQuery(redirectLocation.Query);
+        if (queryStrings.ContainsKey(ReturnUrlQueryStringName))
+        {
+            // Get the plain redirect URL without the query string
+            var redirectWithoutQuery = redirectLocation.ToString().Replace(redirectLocation.Query, string.Empty, StringComparison.Ordinal);
+
+            // Update the query strings to use the original request path as the ReturnUrl query string
+            queryStrings[ReturnUrlQueryStringName] = originalRequestPath.Path.Value ?? "/";
+
+            redirectLocation = new Uri(QueryHelpers.AddQueryString(redirectWithoutQuery, queryStrings));
+        }
+
+        return redirectLocation;
     }
 
     private static Uri UpdateHost(Uri redirectLocation, HttpRequestMessage? authenticationRequest)
