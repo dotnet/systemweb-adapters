f9c74f7e0a71a036c41fcabea2c12423770a8153
Taylor Southwick <tasou@microsoft.com>
2022-08-15 13:24:20 -0700

    Move session serializer

--- src/Microsoft.AspNetCore.SystemWebAdapters/Adapters/IHttpRequestAdapterFeature.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters/Adapters/IHttpRequestAdapterFeature.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if NET6_0_OR_GREATER
+
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.SystemWebAdapters;
@@ -15,3 +17,5 @@ internal interface IHttpRequestAdapterFeature
 
     void ClearContent();
 }
+
+#endif
