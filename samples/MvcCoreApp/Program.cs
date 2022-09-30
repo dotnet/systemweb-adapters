1c9972cef320bf2707ba6cd6fcefdb8567190923
Taylor Southwick <tasou@microsoft.com>
2022-09-30 08:57:29 -0700

    fix paths

--- samples/MvcCoreApp/Program.cs
+++ samples/MvcCoreApp/Program.cs
@@ -34,7 +34,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-
 }
 
 var settings = System.Configuration.ConfigurationManager.AppSettings;
