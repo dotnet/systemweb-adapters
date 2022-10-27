27b1fc3504edc44ba6ba3b572a41b48beabdc8ba
Taylor Southwick <tasou@microsoft.com>
2022-10-27 10:46:00 -0700

    Reorganize to not use internals for core/framework services

--- src/Microsoft.AspNetCore.SystemWebAdapters.FrameworkServices/HostString.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters.FrameworkServices/HostString.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if NETFRAMEWORK
-
 using System;
 using System.Globalization;
 
@@ -211,5 +209,3 @@ internal readonly struct HostString : IEquatable<HostString>
         }
     }
 }
-
-#endif
