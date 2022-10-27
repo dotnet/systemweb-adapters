e777b5731d9b2c7403bc428a4d39be9040e227d5
Taylor Southwick <tasou@microsoft.com>
2022-10-27 10:55:39 -0700

    Move binary writer/reader extensions to framework

--- src/Microsoft.AspNetCore.SystemWebAdapters.FrameworkServices/SessionState/Serialization/BinaryWriterReaderExtensions.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters.FrameworkServices/SessionState/Serialization/BinaryWriterReaderExtensions.cs
@@ -1,10 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if NETFRAMEWORK
-
-using System;
-using System.IO;
 using System.Runtime.CompilerServices;
 
 namespace Microsoft.AspNetCore.SystemWebAdapters.SessionState.Serialization;
@@ -82,4 +78,3 @@ internal static class BinaryWriterReaderExtensions
     }
 }
 
-#endif
