c60bae19cfc2c197b70450dbdd63693f11a8aeee
Taylor Southwick <tasou@microsoft.com>
2022-10-27 12:09:39 -0700

    Add back serializer registration

--- src/Microsoft.AspNetCore.SystemWebAdapters.FrameworkServices/SessionState/RemoteSession/RemoteAppSessionStateExtensions.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters.FrameworkServices/SessionState/RemoteSession/RemoteAppSessionStateExtensions.cs
@@ -19,6 +19,7 @@ public static class RemoteAppSessionStateExtensions
 
         builder.Services.AddScoped<IHttpModule, RemoteSessionModule>();
         builder.Services.AddSingleton<ILockedSessionCache, InMemoryLockedSessions>();
+        builder.Services.AddSingleton<ISessionSerializer, BinarySessionSerializer>();
         builder.Services.AddOptions<SessionSerializerOptions>()
             // We don't want to throw by default on the .NET Framework side as then the error won't be easily visible in the ASP.NET Core app
             .Configure(options => options.ThrowOnUnknownSessionKey = false);
