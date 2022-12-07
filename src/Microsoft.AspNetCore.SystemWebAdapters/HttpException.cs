cf51a752295b0d642f331daa7a5684fd2ee93989
Harry Li <qili8`@microsoft.com>
2022-12-07 16:52:01 +0800

    add more compatible APIs for HttpException

--- src/Microsoft.AspNetCore.SystemWebAdapters/HttpException.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters/HttpException.cs
@@ -1,10 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Net;
+
 namespace System.Web;
 
 public class HttpException : SystemException
 {
+    private readonly int httpStatusCode;
+    
     public HttpException()
     {
     }
@@ -16,5 +21,52 @@ public class HttpException : SystemException
     public HttpException(string message, Exception innerException)
         : base(message, innerException)
     {
+    }
+
+    public HttpException(int httpStatusCode)
+    {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public HttpException(HttpStatusCode httpStatusCode)
+    {
+        this.httpStatusCode = (int)httpStatusCode;
+    }
+
+    public HttpException(int httpStatusCode, string message)
+        : base(message)
+    {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public HttpException(HttpStatusCode httpStatusCode, string message)
+        : base(message)
+    {
+        this.httpStatusCode = (int)httpStatusCode;
+    }
+
+    public HttpException(int httpStatusCode, string message, Exception innerException)
+        : base(message, innerException)
+    {
+        this.httpStatusCode = httpStatusCode;
+    }
+
+    public HttpException(HttpStatusCode httpStatusCode, string message, Exception innerException)
+        : base(message, innerException)
+    {
+        this.httpStatusCode = (int)httpStatusCode;
+    }
+
+    public int GetHttpCode()
+    {
+        return this.httpStatusCode;
+    }
+
+    public int StatusCode
+    {
+        get
+        {
+            return this.httpStatusCode;
+        }
     }
 }
