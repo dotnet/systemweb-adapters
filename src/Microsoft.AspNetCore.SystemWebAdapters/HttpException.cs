6a50402a38c0e4bd439ae3ec57b7300ac86784eb
Maples7 <maples7@163.com>
2022-12-14 00:43:24 +0800

    Add more NETFramework-compatible APIs for HttpException (#254)

--- src/Microsoft.AspNetCore.SystemWebAdapters/HttpException.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters/HttpException.cs
@@ -1,10 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Net;
+
 namespace System.Web;
 
 public class HttpException : SystemException
 {
+    private readonly int _httpStatusCode = 500;
+    
     public HttpException()
     {
     }
@@ -16,5 +21,52 @@ public class HttpException : SystemException
     public HttpException(string message, Exception innerException)
         : base(message, innerException)
     {
+    }
+
+    public HttpException(int httpStatusCode)
+    {
+        _httpStatusCode = httpStatusCode;
+    }
+
+    public HttpException(HttpStatusCode httpStatusCode)
+    {
+        _httpStatusCode = (int)httpStatusCode;
+    }
+
+    public HttpException(int httpStatusCode, string message)
+        : base(message)
+    {
+        _httpStatusCode = httpStatusCode;
+    }
+
+    public HttpException(HttpStatusCode httpStatusCode, string message)
+        : base(message)
+    {
+        _httpStatusCode = (int)httpStatusCode;
+    }
+
+    public HttpException(int httpStatusCode, string message, Exception innerException)
+        : base(message, innerException)
+    {
+        _httpStatusCode = httpStatusCode;
+    }
+
+    public HttpException(HttpStatusCode httpStatusCode, string message, Exception innerException)
+        : base(message, innerException)
+    {
+        _httpStatusCode = (int)httpStatusCode;
+    }
+
+    public int GetHttpCode()
+    {
+        return _httpStatusCode;
+    }
+
+    public int StatusCode
+    {
+        get
+        {
+            return _httpStatusCode;
+        }
     }
 }
---
+++ test/Microsoft.AspNetCore.SystemWebAdapters.Tests/HttpExceptionTests.cs
@@ -0,0 +1,141 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Net;
+using System.Web;
+using AutoFixture;
+using Xunit;
+
+namespace Microsoft.AspNetCore.SystemWebAdapters;
+
+public class HttpExceptionTests
+{
+    [Fact]
+    public void DefaultValues()
+    {
+        // Act
+        var exception = new HttpException();
+
+        // Assert
+        Assert.Equal(500, exception.GetHttpCode());
+    }
+
+    [Fact]
+    public void Create()
+    {
+        // Arrange
+        var message = "message";
+
+        // Act
+        var exception = new HttpException(message);
+
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Equal(500, exception.GetHttpCode());
+    }
+
+    [Fact]
+    public void CreateWithInnerException()
+    {
+        // Arrange
+        var message = "message";
+        var innerException = new Exception();
+
+        // Act
+        var exception = new HttpException(message, innerException);
+
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Equal(innerException, exception.InnerException);
+        Assert.Equal(500, exception.GetHttpCode());
+    }
+
+    [Fact]
+    public void CreateWithHttpStatusCode()
+    {
+        // Arrange
+        var httpStatusCode = 404;
+
+        // Act
+        var exception = new HttpException(httpStatusCode);
+
+        // Assert
+        Assert.Equal(httpStatusCode, exception.GetHttpCode());
+    }
+
+    [Fact]
+    public void CreateWithHttpStatusCodeEnum()
+    {
+        // Arrange
+        var httpStatusCode = HttpStatusCode.NotFound;
+
+        // Act
+        var exception = new HttpException(httpStatusCode);
+
+        // Assert
+        Assert.Equal((int)httpStatusCode, exception.GetHttpCode());
+    }
+
+    [Fact]
+    public void CreateWithHttpStatusCodeAndMessage()
+    {
+        // Arrange
+        var httpStatusCode = 404;
+        var message = "message";
+
+        // Act
+        var exception = new HttpException(httpStatusCode, message);
+
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Equal(httpStatusCode, exception.GetHttpCode());
+    }
+
+    [Fact]
+    public void CreateWithHttpStatusCodeEnumAndMessage()
+    {
+        // Arrange
+        var httpStatusCode = HttpStatusCode.NotFound;
+        var message = "message";
+
+        // Act
+        var exception = new HttpException(httpStatusCode, message);
+
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Equal((int)httpStatusCode, exception.GetHttpCode());
+    }
+
+    [Fact]
+    public void CreateWithIntCodeAndMessageAndInnerException()
+    {
+        // Arrange
+        var httpStatusCode = 404;
+        var message = "message";
+        var innerException = new Exception();
+
+        // Act
+        var exception = new HttpException(httpStatusCode, message, innerException);
+
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Equal(httpStatusCode, exception.GetHttpCode());
+    }
+
+    [Fact]
+    public void CreateWithEnumHttpStatusCodeAndMessageAndInnerException()
+    {
+        // Arrange
+        var httpStatusCode = HttpStatusCode.NotFound;
+        var message = "message";
+        var innerException = new Exception();
+
+        // Act
+        var exception = new HttpException(httpStatusCode, message, innerException);
+
+        // Assert
+        Assert.Equal(message, exception.Message);
+        Assert.Equal((int)httpStatusCode, exception.GetHttpCode());
+    }
+}
