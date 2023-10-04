5d0109001a9468a690114d724472040efbceb459
Taylor Southwick <tasou@microsoft.com>
2023-10-04 11:34:55 -0700

    Enable HttpRequest.PhysicalPath (#410)
    This change just adds the API with a default to null as we don't really have an equivalent for what a physical path is in ASP.NET Core.
    
    Fixes #373

--- src/Microsoft.AspNetCore.SystemWebAdapters/HttpRequest.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters/HttpRequest.cs
@@ -52,6 +52,8 @@ namespace System.Web
         [SuppressMessage("Design", "CA1056:URI-like properties should not be strings", Justification = Constants.ApiFromAspNet)]
         public string RawUrl => _request.HttpContext.Features.GetRequired<IHttpRequestPathFeature>().RawUrl;
 
+        public string? PhysicalPath => _request.HttpContext.Features.GetRequired<IHttpRequestPathFeature>().PhysicalPath;
+
         public string CurrentExecutionFilePath => _request.HttpContext.Features.GetRequired<IHttpRequestPathFeature>().CurrentExecutionFilePath;
 
         public NameValueCollection Headers => _headers ??= _request.Headers.ToNameValueCollection();
