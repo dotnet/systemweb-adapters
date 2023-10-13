4b1d8c364473e949214e6773a108e9914c625857
ruta <ruta@microsoft.com_odspmdb>
2023-10-13 11:31:58 +0800

    update sessionintegrationtests: add tests for sessionattribute and override scenario

--- test/Microsoft.AspNetCore.SystemWebAdapters.CoreServices.Tests/SessionState/SessionIntegrationTests.cs
+++ test/Microsoft.AspNetCore.SystemWebAdapters.CoreServices.Tests/SessionState/SessionIntegrationTests.cs
@@ -3,11 +3,22 @@
 
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Web.SessionState;
+using Autofac.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,12 +29,41 @@ namespace Microsoft.AspNetCore.SystemWebAdapters.CoreServices.Tests.SessionState
 [Collection(nameof(SelfHostedTests))]
 public class SessionIntegrationTests
 {
+    [InlineData("/?override=disabled", "Session:null")]
+    [InlineData("/?override=readonly", "ReadOnly:True")]
+    [InlineData("/?override=required", "ReadOnly:False")]
+    [InlineData("/?override=default", "Session:null")]
+    [Theory]
+    public async Task TestSetSessionStateBehavior(string endpoint, string expected)
+    {
+        var actual = await GetAsync(endpoint);
+        Assert.Equal(expected, actual);
+    }
+
     [InlineData("/disabled", "Session:null")]
     [InlineData("/readonly", "ReadOnly:True")]
     [InlineData("/required", "ReadOnly:False")]
     [InlineData("/default", "Session:null")]
     [Theory]
-    public async Task TestSetSessionStateBehavior(string endpoint, string expected)
+    public async Task TestSessionAttribute(string endpoint, string expected)
+    {
+
+        var actual = await GetAsync(endpoint);
+        Assert.Equal(expected, actual);
+    }
+
+    [InlineData("/disabled?override=required", "ReadOnly:False")]
+    [InlineData("/disabled?override=readonly", "ReadOnly:True")]
+    [InlineData("/readonly?override=required", "ReadOnly:False")]
+    [InlineData("/default?override=disabled", "Session:null")]
+    [Theory]
+    public async Task TestOverrideSessionStateBehavior(string endpoint, string expected)
+    {
+        var actual = await GetAsync(endpoint);
+        Assert.Equal(expected, actual);
+    }
+
+    private static async Task<string> GetAsync(string endpoint)
     {
         using var host = await new HostBuilder()
           .ConfigureWebHost(webBuilder =>
@@ -36,6 +76,7 @@ public class SessionIntegrationTests
                   .ConfigureServices(services =>
                   {
                       services.AddRouting();
+                      services.AddControllers();
                       services.AddSystemWebAdapters()
                         .WrapAspNetCoreSession();
                       services.AddDistributedMemoryCache();
@@ -45,69 +86,48 @@ public class SessionIntegrationTests
                       app.UseRouting();
                       app.Use((ctx, next) =>
                       {
-                          SetSessionState(ctx);
+                          SetOverrideSessionState(ctx);
                           return next(ctx);
                       });
                       app.UseSession();
                       app.UseSystemWebAdapters();
                       app.UseEndpoints(endpoints =>
                       {
-                          endpoints.Map("/{*url}", (context) => MyController(context));
+                          endpoints.MapControllers();
 
                       });
                   });
           })
           .StartAsync();
 
-        using var client = host.GetTestClient();
+        var uri = new Uri(endpoint, UriKind.Relative);
 
-        // Act
-        var result = await GetResponseAsync(client, endpoint);
-
-        // Assert
-        Assert.Equal(expected, result);
-    }
-
-    private static Task MyController(HttpContext context)
-    {
-        var session = context.Session;
-        if (session == null)
+        try
         {
-            context.Response.Write("Session:null");
+            return await host.GetTestClient().GetStringAsync(uri).ConfigureAwait(false);
         }
-        else
+        finally
         {
-            context.Response.Write($"ReadOnly:{session.IsReadOnly}"); ;
+            await host.StopAsync();
         }
-
-        return Task.CompletedTask;
     }
 
-    private static async Task<string> GetResponseAsync(HttpClient client, string uri)
+    private static void SetOverrideSessionState(HttpContext context)
     {
-        using var message = new HttpRequestMessage(HttpMethod.Get, new Uri(uri, UriKind.Relative));
-        using var response = await client.SendAsync(message);
+        string? overrideValue = context.Request.QueryString["override"];
 
-        return await response.Content.ReadAsStringAsync();
-    }
-
-
-    private static void SetSessionState(HttpContext context)
-    {
-        var path = context.Request.Path;
-
-        switch (path)
+        switch (overrideValue)
         {
-            case "/disabled":
+            case "disabled":
                 context.SetSessionStateBehavior(SessionStateBehavior.Disabled);
                 break;
-            case "/readonly":
+            case "readonly":
                 context.SetSessionStateBehavior(SessionStateBehavior.ReadOnly);
                 break;
-            case "/required":
+            case "required":
                 context.SetSessionStateBehavior(SessionStateBehavior.Required);
                 break;
-            case "/default":
+            case "default":
                 context.SetSessionStateBehavior(SessionStateBehavior.Default);
                 break;
             default:
@@ -115,3 +135,61 @@ public class SessionIntegrationTests
         }
     }
 }
+
+[ApiController]
+public class SessionStatusController : ControllerBase
+{
+    [HttpGet]
+    [Route("/")]
+    public ActionResult<string> Get()
+    {
+        return Ok(GetSessionStatusString(System.Web.HttpContext.Current));
+    }
+
+    [HttpGet]
+    [Session(SessionBehavior = SessionStateBehavior.Disabled)]
+    [Route("/disabled")]
+    public ActionResult<string> GetDisabled()
+    {
+        return Ok(GetSessionStatusString(System.Web.HttpContext.Current));
+    }
+
+    [HttpGet]
+    [Session(SessionBehavior = SessionStateBehavior.ReadOnly)]
+    [Route("/readonly")]
+    public ActionResult<string> GetReadOnly()
+    {
+        return Ok(GetSessionStatusString(System.Web.HttpContext.Current));
+    }
+
+    [HttpGet]
+    [Session(SessionBehavior = SessionStateBehavior.Required)]
+    [Route("/required")]
+    public ActionResult<string> GetRequired()
+    {
+        return Ok(GetSessionStatusString(System.Web.HttpContext.Current));
+    }
+
+    [HttpGet]
+    [Session(SessionBehavior = SessionStateBehavior.Default)]
+    [Route("/default")]
+    public ActionResult<string> GetDefault()
+    {
+        return Ok(GetSessionStatusString(System.Web.HttpContext.Current));
+    }
+
+    public static string GetSessionStatusString(HttpContext context)
+    {
+        var session = context.Session;
+        if (session == null)
+        {
+            return "Session:null";
+        }
+        else
+        {
+            return $"ReadOnly:{session.IsReadOnly}";
+        }
+
+    }
+}
+
