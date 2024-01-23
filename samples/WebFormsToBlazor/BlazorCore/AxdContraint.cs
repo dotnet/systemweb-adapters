0a5f28f49a9f3f0b7ccc5d242866cff90071c99c
Andrew Jank <Andrew.Jank@etapro.com>
2024-01-23 15:27:16 -0500

    combined new sample into old sample, updated README for WebFormsToBlazor sample

--- samples/WebFormsToBlazor/BlazorCore/AxdContraint.cs
+++ samples/WebFormsToBlazor/BlazorCore/AxdContraint.cs
@@ -1,7 +1,7 @@
 using System.Globalization;
 using System.Text.RegularExpressions;
 
-namespace BlazorSessionCore
+namespace BlazorCore
 {
     internal sealed class AxdContraint : IRouteConstraint
     {
