d3f383bb1d05a12c8bcedf0581341954831af935
Andrew Jank <Andrew.Jank@etapro.com>
2024-01-23 15:27:16 -0500

    combined new sample into old sample, updated README for WebFormsToBlazor sample

--- samples/WebFormsToBlazor/BlazorCore/Program.cs
+++ samples/WebFormsToBlazor/BlazorCore/Program.cs
@@ -1,27 +1,47 @@
 using BlazorCore;
-using BlazorCore.Data;
+using BlazorCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSystemWebAdapters();
-builder.Services.AddReverseProxy();
+// Add YARP
+builder.Services.AddHttpForwarder();
+
+// Add constraint to redirect .axd files to WebForms
+builder.Services.AddRouting(options => options.ConstraintMap.Add("isAxdFile", typeof(AxdContraint)));
+// Add System Web Adapters and setup session
+builder.Services.AddSystemWebAdapters()
+    .AddJsonSessionSerializer(options =>
+    {
+        options.RegisterKey<string>("test-value");
+    })
+    .AddRemoteAppClient(options =>
+    {
+        options.RemoteAppUrl = new(builder.Configuration["ProxyTo"]);
+        options.ApiKey = builder.Configuration["RemoteAppApiKey"];
+    })
+    .AddSessionClient();
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor(options =>
-{
-    options.RootComponents.RegisterCustomElement<HelloWorld>("hello-world");
-});
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents(options => options.RootComponents.RegisterCustomElement<HelloWorld>("hello-world"));
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
-app.UseRouting();
+app.UseAntiforgery();
+app.UseSystemWebAdapters();
 
-app.MapBlazorHub();
-app.MapBlazorPages("/_Host");
-app.MapForwarder("/{**catch-all}", app.Configuration["ProxyTo"]!).WithOrder(int.MaxValue);
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .RequireSystemWebAdapterSession();
 
+app.MapForwarder("/Scripts/{**catch-all}", app.Configuration["ProxyTo"]).Add(static builder => ((RouteEndpointBuilder)builder).Order = int.MaxValue);
+app.MapForwarder("/Content/{**catch-all}", app.Configuration["ProxyTo"]).Add(static builder => ((RouteEndpointBuilder)builder).Order = int.MaxValue);
+app.MapForwarder("/bundles/{**catch-all}", app.Configuration["ProxyTo"]).Add(static builder => ((RouteEndpointBuilder)builder).Order = int.MaxValue);
+app.MapForwarder("/About", app.Configuration["ProxyTo"]).Add(static builder => ((RouteEndpointBuilder)builder).Order = int.MaxValue);
+app.MapForwarder("/Contact", app.Configuration["ProxyTo"]).Add(static builder => ((RouteEndpointBuilder)builder).Order = int.MaxValue);
+app.MapForwarder("/{route:isAxdFile}", app.Configuration["ProxyTo"]).Add(static builder => ((RouteEndpointBuilder)builder).Order = int.MaxValue);
 app.Run();
--- samples/WebFormsToBlazor/WebFormsFramework/Global.asax.cs
+++ samples/WebFormsToBlazor/WebFormsFramework/Global.asax.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 using System.Web;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -9,13 +12,22 @@ namespace WebFormsFramework
     {
         void Application_Start(object sender, EventArgs e)
         {
-            SystemWebAdapterConfiguration.AddSystemWebAdapters(this)
-                .AddVirtualizedContentDirectories()
-                .AddProxySupport(options => options.UseForwardedHeaders = true);
-
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            SystemWebAdapterConfiguration.AddSystemWebAdapters(this)
+                .AddVirtualizedContentDirectories()
+                .AddProxySupport(options => options.UseForwardedHeaders = true)
+                .AddJsonSessionSerializer(options =>
+                {
+                    options.RegisterKey<string>("test-value");
+                })
+                .AddRemoteAppServer(options =>
+                {
+                    options.ApiKey = ConfigurationManager.AppSettings["RemoteAppApiKey"];
+                })
+                .AddSessionServer();
         }
     }
 }
