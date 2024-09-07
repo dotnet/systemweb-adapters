0e371c6a0c9744f47e5d01b297670a57a864121f
Andrew Jank <Andrew.Jank@etapro.com>
2024-09-07 18:19:58 -0400

    Revert "combined new sample into old sample, updated README for WebFormsToBlazor sample"
    This reverts commit 94248b9c332550ca304e5d23f144bc874eb1d6be.

---
+++ samples/WebFormsToBlazorWithSession/WebFormsSessionFramework/Contact.aspx.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsSessionFramework
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                SessionTextBox.Text = Session["test-value"]?.ToString() ?? string.Empty;
+            }
+        }
+
+        protected void SubmitSessionButton_Click(object sender, EventArgs e)
+        {
+            Session["test-value"] = SessionTextBox.Text;
+        }
+    }
+}
