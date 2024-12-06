db889ac6bf9f911bfd6fa55f91f291d5b5cf691a
Taylor Southwick <twsouthwick@outlook.com>
2024-12-05 19:09:38 -0800

    Add remote session e2e sample

---
+++ samples/RemoteSession/RemoteSessionFramework/Global.asax.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Configuration;
+using System.Web;
+using System.Web.Routing;
+
+namespace RemoteSessionFramework
+{
+    public class SessionApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            SystemWebAdapterConfiguration.AddSystemWebAdapters(this)
+                .AddProxySupport(options => options.UseForwardedHeaders = true)
+                .AddSessionSerializer(options =>
+                {
+                })
+                .AddJsonSessionSerializer(options =>
+                {
+                    options.RegisterKey<int>("CoreCount");
+                })
+                .AddRemoteAppServer(options => options.ApiKey = ConfigurationManager.AppSettings["RemoteAppApiKey"])
+                .AddSessionServer(options =>
+                {
+                });
+        }
+
+        protected void Application_PostAcquireRequestState(object sender, EventArgs e)
+        {
+            if (((HttpApplication)sender).Context.Session is { } session)
+            {
+                if (session["FrameworkCount"] is int count)
+                {
+                    session["FrameworkCount"] = count + 1;
+                }
+                else
+                {
+                    session["FrameworkCount"] = 0;
+                }
+            }
+        }
+    }
+}
