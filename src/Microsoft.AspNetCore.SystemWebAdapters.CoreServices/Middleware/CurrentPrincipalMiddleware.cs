15ba1e513a2ae71667876c8c9cddb2cd4250dcc0
Taylor Southwick <tasou@microsoft.com>
2025-11-11 12:18:58 -0800

    clean up

--- src/Microsoft.AspNetCore.SystemWebAdapters.CoreServices/Middleware/CurrentPrincipalMiddleware.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters.CoreServices/Middleware/CurrentPrincipalMiddleware.cs
@@ -1,33 +1,22 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.SystemWebAdapters.Features;
-using Microsoft.Extensions.Logging;
 
 namespace Microsoft.AspNetCore.SystemWebAdapters.Middleware;
 
-internal sealed class CurrentPrincipalMiddleware
+internal sealed class CurrentPrincipalMiddleware(RequestDelegate next)
 {
-    private readonly RequestDelegate _next;
-    private readonly ILogger<CurrentPrincipalMiddleware> _logger;
-
-    public CurrentPrincipalMiddleware(RequestDelegate next, ILogger<CurrentPrincipalMiddleware> logger)
-    {
-        _next = next;
-        _logger = logger;
-    }
-
-    public Task InvokeAsync(HttpContext context)
+    public Task InvokeAsync(HttpContextCore context)
     {
         if (context.GetEndpoint()?.Metadata.GetMetadata<SetThreadCurrentPrincipalAttribute>() is { IsDisabled: false })
         {
             context.Features.GetRequiredFeature<IRequestUserFeature>().EnableStaticAccessors();
         }
 
-        return _next(context);
+        return next(context);
     }
 }
