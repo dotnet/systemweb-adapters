1d42c22de87a99a353941ba0e0ce06cb3099e2ec
Taylor Southwick <tasou@microsoft.com>
2025-11-12 11:55:56 -0800

    alias

--- src/Microsoft.AspNetCore.SystemWebAdapters.Owin/AppBuilderDataProtectionExtensions.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters.Owin/AppBuilderDataProtectionExtensions.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Interop;
 using Owin;
 
 using AspNetCoreDataProtectionProvider = Microsoft.AspNetCore.DataProtection.IDataProtectionProvider;
+using OwinProtectionProvider = Microsoft.Owin.Security.DataProtection.IDataProtectionProvider;
 
 namespace Microsoft.Owin.Security.DataProtection;
 
@@ -19,7 +20,7 @@ public static class AppBuilderDataProtectionExtensions
         app.SetDataProtectionProvider(new DataProtectionProviderShim(dataProtectionProvider));
     }
 
-    private sealed class DataProtectionProviderShim(AspNetCoreDataProtectionProvider other) : IDataProtectionProvider
+    private sealed class DataProtectionProviderShim(AspNetCoreDataProtectionProvider other) : OwinProtectionProvider
     {
         public IDataProtector Create(params string[] purposes) => new DataProtectorShim(other.CreateProtector(purposes));
     }
