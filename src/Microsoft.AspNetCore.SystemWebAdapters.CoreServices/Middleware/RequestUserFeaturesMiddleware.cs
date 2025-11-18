7ef1062e52556bcb7072141b373671c98ae1aa91
Taylor Southwick <tasou@microsoft.com>
2025-11-18 11:42:01 -0800

    Fix issue when enabling static user accessors
    The RequestUserFeature implements the ASP.NET Core IHttpAuthenticationFeature as well as the adapter's IRequestUserFeature. We were only copying the user to the static accessors when the ASP.NET Core User was set not when the IRequestUserFeature one was. Since there's a single User property that stores it for both, this change moves the call to sync to the static accessors there.

--- src/Microsoft.AspNetCore.SystemWebAdapters.CoreServices/Middleware/RequestUserFeaturesMiddleware.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters.CoreServices/Middleware/RequestUserFeaturesMiddleware.cs
@@ -19,7 +19,7 @@ internal sealed partial class RequestUserFeaturesMiddleware(RequestDelegate next
     private readonly ILogger _logger = loggerFactory.CreateLogger<RequestUserFeature>();
     private readonly bool _setCurrentAccessors = options.Value.EnableStaticUserAccessors;
 
-    public async Task InvokeAsync(HttpContextCore context, IOptions<SystemWebAdaptersOptions> options)
+    public async Task InvokeAsync(HttpContextCore context)
     {
         using var userFeature = new RequestUserFeature(_logger, _setCurrentAccessors) { User = context.User };
 
@@ -47,18 +47,22 @@ internal sealed partial class RequestUserFeaturesMiddleware(RequestDelegate next
         [LoggerMessage(1, LogLevel.Trace, "Thread.CurrentPrincipal has been set with the current user")]
         private partial void LogCurrentPrincipalUsage();
 
-        public IPrincipal? User { get; set; }
+        public IPrincipal? User
+        {
+            get;
+            set
+            {
+                field = value;
+                EnsureCurrentPrincipalSetIfRequired();
+            }
+        }
 
         WindowsIdentity? IRequestUserFeature.LogonUserIdentity => User?.Identity as WindowsIdentity;
 
         ClaimsPrincipal? IHttpAuthenticationFeature.User
         {
             get => GetOrCreateClaims(User);
-            set
-            {
-                User = value;
-                EnsureCurrentPrincipalSetIfRequired();
-            }
+            set => User = value;
         }
 
         private ClaimsPrincipal? GetOrCreateClaims(IPrincipal? principal)
