c1ba146ae648334eeca145808f380c169c90baff
Taylor Southwick <tasou@microsoft.com>
2025-12-03 13:22:07 -0800

    rename scheme

--- src/Microsoft.AspNetCore.SystemWebAdapters.CoreServices/Authentication/RemoteAppAuthenticationExtensions.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters.CoreServices/Authentication/RemoteAppAuthenticationExtensions.cs
@@ -96,7 +96,7 @@ public static class RemoteAppAuthenticationExtensions
     /// </summary>
     private static void AddEmptyAuthenticationScheme(this AuthenticationBuilder authenticationBuilder)
     {
-        authenticationBuilder.AddScheme<AuthenticationSchemeOptions, EmptyAuthenticationHandler>(Guid.NewGuid().ToString(), _ => { });
+        authenticationBuilder.AddScheme<AuthenticationSchemeOptions, EmptyAuthenticationHandler>("__SystemWebAdapters_NotForUse__", _ => { });
     }
 
     private sealed class EmptyAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
