47b1e9255bcd059701be537d72df1e0dd57b9395
Taylor Southwick <tasou@microsoft.com>
2025-12-18 11:16:43 -0800

    organize usings

--- samples/AuthRemoteIdentity/AuthRemoteIdentityFramework/Models/IdentityModels.cs
+++ samples/AuthRemoteIdentity/AuthRemoteIdentityFramework/Models/IdentityModels.cs
@@ -1,10 +1,10 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace MvcApp.Models
 {
-    using Microsoft.AspNet.Identity;
-    using Microsoft.AspNet.Identity.EntityFramework;
-
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
