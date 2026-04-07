58cc680e351bbc5c1d7ad0fe840a2759fb1ca76a
Taylor Southwick <twsouthwick@outlook.com>
2026-04-07 15:58:34 -0700

    Bring Microsoft.AspNetCore.DataProtection.SystemWeb compatibility mode into the FrameworkServices project

---
+++ src/Microsoft.AspNetCore.SystemWebAdapters.FrameworkServices/MachineKeyExtensions.cs
@@ -0,0 +1,29 @@
+// L
+// icensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Reflection;
+using System.Web.Configuration;
+
+namespace System.Web;
+
+internal static class MachineKeyExtensions
+{
+
+    private static FieldInfo _configField = typeof(MachineKeySection).GetField("s_config", BindingFlags.NonPublic | BindingFlags.Static);
+    private static MethodInfo _ensureConfig = typeof(MachineKeySection).GetMethod("EnsureConfig", BindingFlags.NonPublic | BindingFlags.Static);
+    private static MethodInfo _getApplicationConfig = typeof(MachineKeySection).GetMethod("GetApplicationConfig", BindingFlags.NonPublic | BindingFlags.Static);
+
+    extension(MachineKeySection section)
+    {
+        internal static MachineKeySection Value
+        {
+            get => (MachineKeySection)_configField.GetValue(null);
+            set => _configField.SetValue(null, value);
+        }
+
+        internal static void EnsureConfig() => _ensureConfig.Invoke(null, []);
+
+        internal static MachineKeySection GetApplicationConfig() => (MachineKeySection)_getApplicationConfig.Invoke(null, []);
+    }
+}
--- src/Microsoft.AspNetCore.SystemWebAdapters.FrameworkServices/SystemWebDataProtectionExtensions.cs
+++ src/Microsoft.AspNetCore.SystemWebAdapters.FrameworkServices/SystemWebDataProtectionExtensions.cs
@@ -1,19 +1,21 @@
-// Licensed to the .NET Foundation under one or more agreements.
+// L
+// icensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Configuration;
 using System.Web.Configuration;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.Infrastructure;
 using Microsoft.AspNetCore.DataProtection.SystemWeb;
 using Microsoft.AspNetCore.SystemWebAdapters.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 
 namespace System.Web;
 
 public static class SystemWebDataProtectionExtensions
 {
-    private const string StartupTypeKey = "aspnet:dataProtectionStartupType";
-
     public static IDataProtectionBuilder AddDataProtection(this HttpApplicationHostBuilder builder)
     {
         if (builder is null)
@@ -21,61 +23,87 @@ public static class SystemWebDataProtectionExtensions
             throw new ArgumentNullException(nameof(builder));
         }
 
-        if (!IsMachineKeyOverriden())
-        {
-            const string ExpectedSetup = """
-                <machineKey compatibilityMode="Framework45" dataProtectorType="Microsoft.AspNetCore.DataProtection.SystemWeb.CompatibilityDataProtector, Microsoft.AspNetCore.DataProtection.SystemWeb" />
-                """;
-            throw new InvalidOperationException($"Must configure machine key in web.config to use data protection: {ExpectedSetup}");
-        }
+        TrySetMachineKeyOverride();
 
-        if (!TrySetStartupType())
-        {
-            throw new InvalidOperationException($"Must not manually set the '{StartupTypeKey}' app setting when using HttpApplicationHostBuilder.AddDataProtection.");
-        }
+        builder.Services.TryAddSingleton<IApplicationDiscriminator, SystemWebApplicationDiscriminator>();
 
         return builder.Services.AddDataProtection();
     }
 
-    private static bool TrySetStartupType()
+
+    private static bool _isSet;
+
+    private static void TrySetMachineKeyOverride()
     {
-        var startupType = typeof(MachineKeyImpl).AssemblyQualifiedName;
-
-        var current = ConfigurationManager.AppSettings[StartupTypeKey];
-
-        if (current != startupType && !string.IsNullOrEmpty(current))
+        if (_isSet)
         {
-            return false;
+            return;
         }
 
-        ConfigurationManager.AppSettings[StartupTypeKey] = startupType;
+        _isSet = true;
 
-        return true;
-    }
-
-    private static bool IsMachineKeyOverriden()
-    {
         if (ConfigurationManager.GetSection("system.web/machineKey") is MachineKeySection section)
         {
-            if (section.CompatibilityMode != MachineKeyCompatibilityMode.Framework45)
+            if (!string.IsNullOrEmpty(section.DataProtectorType))
             {
-                return false;
+                throw new InvalidOperationException("Could not setup DataProtection for use with MachineKey due to existing configuration of system.web/machineKey");
             }
 
-            if (section.DataProtectorType is { } typeString && Type.GetType(typeString) is { } type && type == typeof(CompatibilityDataProtector))
+            if (section.CompatibilityMode != MachineKeyCompatibilityMode.Framework45)
             {
-                return true;
+                throw new InvalidOperationException($"Could not setup DataProtection for use with MachineKey due to invalid CompatibilityMode ({section.CompatibilityMode})");
             }
         }
 
-        return false;
+        var existing = MachineKeySection.GetApplicationConfig();
+        var updated = new MachineKeySection()
+        {
+            ApplicationName = existing.ApplicationName,
+            CompatibilityMode = MachineKeyCompatibilityMode.Framework45,
+            DataProtectorType = typeof(CompatibilityDataProtector).AssemblyQualifiedName,
+            Decryption = existing.Decryption,
+            DecryptionKey = existing.DecryptionKey,
+        };
+
+        MachineKeySection.Value = updated;
+
+#if DEBUG
+        var newAppConfig = MachineKeySection.GetApplicationConfig();
+        MachineKeySection.EnsureConfig();
+        var afterEnsureConfig = MachineKeySection.GetApplicationConfig();
+
+        if (!object.ReferenceEquals(updated, newAppConfig) || !ReferenceEquals(updated, afterEnsureConfig))
+        {
+            throw new InvalidOperationException("MachineKey was overwritten");
+        }
+#endif
     }
 
-    private sealed class MachineKeyImpl : DataProtectionStartup
+    private sealed class SystemWebApplicationDiscriminator : IApplicationDiscriminator
     {
-        public override IDataProtectionProvider CreateDataProtectionProvider(IServiceProvider services)
+        private readonly Lazy<string> _lazyDiscriminator;
+
+        public SystemWebApplicationDiscriminator(IHostEnvironment env)
         {
-            return base.CreateDataProtectionProvider(HttpApplicationHost.Current.Services);
+            _lazyDiscriminator = new Lazy<string>(() => GetAppDiscriminatorCore(env));
+        }
+
+        public string Discriminator => _lazyDiscriminator.Value;
+
+        private static string GetAppDiscriminatorCore(IHostEnvironment env)
+        {
+            // Try reading the discriminator from <machineKey applicationName="..." /> defined
+            // at the web app root. If the value was set explicitly (even if the value is empty),
+            // honor it as the discriminator.
+            var machineKeySection = (MachineKeySection)WebConfigurationManager.GetWebApplicationSection("system.web/machineKey");
+            if (machineKeySection.ElementInformation.Properties["applicationName"].ValueOrigin != PropertyValueOrigin.Default)
+            {
+                return machineKeySection.ApplicationName;
+            }
+            else
+            {
+                return env.ApplicationName;
+            }
         }
     }
 }
